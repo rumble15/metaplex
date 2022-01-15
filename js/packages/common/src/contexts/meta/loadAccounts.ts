07faa0cc10a09fe07440a904666d11d61359f979
rumble15 <Github@hein.cf>
2022-01-15 19:21:50 +0000

    master

--- js/packages/common/src/contexts/meta/loadAccounts.ts
+++ js/packages/common/src/contexts/meta/loadAccounts.ts
@@ -58,9 +58,9 @@ import { processPackCards } from './processPackCards';
 import { getProvingProcessByPackSetAndWallet } from '../../models/packs/accounts/ProvingProcess';
 import { processProvingProcess } from './processProvingProcess';
 
-const MULTIPLE_ACCOUNT_BATCH_SIZE = 100;
+const MULTIPLE_ACCOUNT_BATCH_SIZE = 10;
 
-export const USE_SPEED_RUN = false;
+export const USE_SPEED_RUN = true;
 const WHITELISTED_METADATA = ['98vYFjBYS9TguUMWQRPjy2SZuxKuUMcqR4vnQiLjZbte'];
 const WHITELISTED_AUCTION = ['D8wMB5iLZnsV7XQjpwqXaDynUtFuDs7cRXvEGNj1NF1e'];
 const AUCTION_TO_METADATA: Record<string, string[]> = {
